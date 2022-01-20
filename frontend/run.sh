9605084983aed9f63de2350f3e71ef484d8b0219
Danyel-Hirose <danyyhm@hotmail.com>
2022-01-20 01:35:12 -0300

    Workflow para web
    Workflow para web

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,5 +1,16 @@
 robot -l NONE -o NONE -r NONE tasks/Delorean.robot
-pabot -d ./logs -v BROWSER:chromium -v HEADLESS:False  tests
+pabot -x xunit.xml -d ./logs -v BROWSER:chromium -v HEADLESS:False  tests
+
+
+# Dando um jeito na Evidencia:
+rm -rf ./logs/browser
+mkdir ./logs/browser
+mkdir ./logs/browser/screenshot
+
+cp $(find ./logs/pabot_results -type f -name "*.png") ./logs/browser/screenshot/
+
+
+#pabot -d ./logs -v BROWSER:chromium -v HEADLESS:False  tests
 #pabot -d ./logs -v BROWSER:chromium -v HEADLESS:True -i smoke tests
 #robot -d ./logs tests
 #robot -d ./logs tests/Attempt_BeGeek.robot
@@ -8,12 +19,3 @@ pabot -d ./logs -v BROWSER:chromium -v HEADLESS:False  tests
 #robot -d ./logs tests/Login.robot
 #robot -d ./logs tests/Signup.robot
 #robot -d ./logs -i DDD tests/BeGeek.robot 
-
-
-
-
-# Dando um jeito na Evidencia:
-rm -rf ./logs/browser
-mkdir ./logs/browser
-mkdir ./logs/browser/screenshot
-cp $(find ./logs/pabot_results -type f -name "*.png") ./logs/browser/screenshot/
